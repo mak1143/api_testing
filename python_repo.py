ae2f8b037255fc91c24984333d95aaca1a2d4d66
mak1143 <206786647+mak1143@users.noreply.github.com>
2026-07-28 15:11:38 +0000

    new features

---
+++ python_repo.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+
+
+headers = {"Accept": "application/vnd.github.v3+json"}
+r = requests.get(url, headers=headers)
+print(f"status code: {r.status_code}")
+
+# store api response in a variable
+response_dict = r.json()
+
+
+# process results
+print(response_dict.keys())
